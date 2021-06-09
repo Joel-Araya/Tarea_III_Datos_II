0eb1c4b990bfc3cae40d8597c10aabc8c7146c61
Joel Gómez <62965696+Joel-Araya@users.noreply.github.com>
2021-06-09 17:12:15 -0600

    Creación del main

---
+++ cmake-build-debug/CMakeFiles/Tarea_III_Datos_II.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Tarea_III_Datos_II.dir/main.cpp.obj"
+  "Tarea_III_Datos_II.exe"
+  "Tarea_III_Datos_II.exe.manifest"
+  "Tarea_III_Datos_II.pdb"
+  "libTarea_III_Datos_II.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Tarea_III_Datos_II.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/Tarea_III_Datos_II.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Tarea_III_Datos_II.
+# This may be replaced when dependencies are built.
